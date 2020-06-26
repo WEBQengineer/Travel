64d1485ee0652bca85b4215d7f16adb1db8cfebc
ZQ C <zqc@192.168.0.109>
2020-06-26 12:46:19 +0800

    常规提交

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    line-height: .86rem
+    background: #00bcd4
+    color: #fff
+    display: flex
+    .header-left
+      width: .64rem
+      float: left
+      .back-icon
+        text-align: center
+        font-size : .4rem
+    .header-right
+      width: 1.24rem
+      text-align: center
+      float: right
+      .arrow-icon
+        font-size: .24rem
+    .header-input
+      background: #fff
+      color: #ccc
+      padding-left: .1rem
+      margin-top: .12rem
+      margin-left: .1rem
+      height: .62rem
+      line-height: .62rem
+      border-radius: .1rem
+      flex: 1
+</style>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swaper-img"  :src="item.imgUrl" />
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/e52c3e98602b90f198ec316dce253cba.jpg?thumb=1&w=2452&h=920&f=webp&q=90'
+      }, {
+        id: '0002',
+        imgUrl: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/29d0a0b83843d7d1ba7a30a9400257e9.jpg?thumb=1&w=2452&h=920&f=webp&q=90'
+      }, {
+        id: '0003',
+        imgUrl: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/6ef43cf9f138a7fc3a39273d7e3d13b6.jpg?thumb=1&w=2452&h=920&f=webp&q=90'
+      }]
+    }
+  }
+  // data: function () {
+  //   return {
+  //     swiperOption: {}
+  //   }
+  // }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 38%
+    background: #eee
+    .swaper-img
+      width: 100%
+</style>
