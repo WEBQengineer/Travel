8172810bea9995926100cb9e591f217dcb498961
ZQ C <zqc@192.168.0.109>
2020-07-02 21:54:30 +0800

    icon第一版样式

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="icons">
-    
+    <div class="icon">
+      <div class="iconimg">
+        <img class="icon-img-content" src="//s.qunarzz.com/homenode/images/touchheader/hotel.png"/>
+      </div>
+      <p class="icon-desc">酒店</p>
+    </div>
   </div>
 </template>
 
@@ -15,5 +20,29 @@ export default {
     overflow: hidden
     height: 0
     padding-bottom: 50%
-    background: green
+    .icon
+      position: relative
+      height: 0
+      width: 25%
+      padding-bottom: 25%
+      .iconimg
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        box-sizing: border-box
+        padding: .1rem
+        .icon-img-content
+          display: block
+          margin: 0 auto
+          height: 100%
+      .icon-desc
+        text-align: center
+        position: absolute
+        bottom: 0
+        left: 0
+        right: 0
+        height: .44rem
+        line-height: .44rem
 </style>
