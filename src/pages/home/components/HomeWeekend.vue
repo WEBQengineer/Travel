d4bbe15a419982e47b4c1a1b89519d1cbedfdb97
ZQ C <zqc@192.168.0.109>
2020-07-04 22:14:29 +0800

    完成推荐、周末去哪

---
+++ src/pages/home/components/HomeWeekend.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <div class="itemAll" v-for="item of itemList" :key="item.id">
+      <div class="imgCon">
+        <img class="weekendimg" :src="item.imgUrl" />
+      </div>
+      <p class="iteminfo" >{{item.info}}</p>
+      <p class="itemdesc" >{{item.desc}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      itemList: [
+        {
+          id: '0001',
+          imgUrl: '//img1.qunarzz.com/sight/p0/2005/5d/5d51e25c5812fef6a3.water.jpg_256x160_4fec2721.jpg',
+          info: '天津之眼摩天轮',
+          desc: '华灯初上，享受别样浪漫华灯初上'
+        }, {
+          id: '0002',
+          imgUrl: '//img1.qunarzz.com/sight/p0/2005/5d/5d51e25c5812fef6a3.water.jpg_256x160_4fec2721.jpg',
+          info: '天津之眼摩天轮',
+          desc: '华灯初上，享受别样浪漫华灯初上'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    line-height: .7rem
+    background: #eee
+  .itemAll
+    margin: .05rem
+    .imgCon
+      overflow: hidden
+      height: 0
+      padding-bottom: 33.9%
+      border-radius: .05rem
+      .weekendimg
+        width: 100%
+    .iteminfo
+      line-height: .5rem
+      font-size: .32rem
+      ellipsis()
+    .itemdesc
+      color: #ccc
+      line-height: .5rem
+      font-size: .28rem
+      ellipsis()
+</style>
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of itemList" :key="item.id">
+        <img class="item-img" :src="item.imgUrl" />
+        <div class="item-info">
+          <p class="item-title">{{item.itemtitle}}</p>
+          <p class="item-desc">{{item.itemdesc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      itemList: [
+        {
+          id: '0001',
+          imgUrl: '//s.qunarzz.com/homenode/images/touchheader/package.png',
+          itemtitle: '大连圣亚海洋世界大连圣亚海洋世界大连圣亚海洋世界大连圣亚海洋世界',
+          itemdesc: '浪漫大连首站，浪漫的海洋主题乐园浪漫大连首站，浪漫的海洋主题乐园浪漫大连首站，浪漫的海洋主题乐园'
+        }, {
+          id: '0001',
+          imgUrl: '//s.qunarzz.com/homenode/images/touchheader/package.png',
+          itemtitle: '大连圣亚海洋世界',
+          itemdesc: '浪漫大连首站，浪漫的海洋主题乐园'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .recommend-title
+    margin-top: .2rem
+    line-height: .7rem
+    text-indent: .2rem
+    background: #eee
+  .item
+    display: flex
+    overflow: hidden
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .3rem
+        border-radius: .06rem
+        color: #fff
+</style>
