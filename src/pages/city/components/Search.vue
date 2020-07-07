a28337f60e7bdca51b4968f02dc9cf5803ac412b
ZQ C <zqc@192.168.0.109>
2020-07-07 21:42:05 +0800

    完成City页搜索框

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+      <input class="search-input" placeholder="请输入城市名或者拼音" type="text" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .search
+    height: .62rem
+    background: $bgColor
+    padding: .1rem .1rem
+    .search-input
+      height: 100%
+      width: 100%
+      line-height: 100%
+      box-sizing: border-box
+      text-align: center
+      padding: 0 .1rem
+      color: #666
+      border-radius: .06rem
+</style>
