74ff73f869ab6934b7f02b3eb05c8b7c60a4766a
ZQ C <zqc@192.168.0.109>
2020-07-14 22:12:50 +0800

    git完成头部渐隐渐显

---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <router-link to="/" class="header-abs" v-show="showAbs">
+        <div class="iconfont header-abs-back">&#xe624;</div>
+    </router-link>
+    <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">景点详情</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailHeader',
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    handleScroll () {
+      const scrp = document.documentElement.scrollTop
+      if (scrp>46) {
+        let opacity = scrp / 80 
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = {opacity}
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header-abs
+    position: absolute
+    left: .2rem
+    top: .2rem
+    width: .7rem
+    height: .7rem
+    line-height: .7rem
+    border-radius: .35rem
+    text-align: center
+    background: rgba(0, 0, 0, .8)
+    .header-abs-back
+      color: #fff
+      font-size: .35rem
+  .header-fixed
+    position: fixed
+    top: 0
+    left: 0
+    right: 0
+    height: $headerHeight
+    line-height: $headerHeight
+    text-align: center
+    color: #fff
+    background: $bgColor
+    font-size: .32rem
+</style>
