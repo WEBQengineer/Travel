c1e2be2ac51aa9e92ece352f5c28d19c5ea86342
ZQ C <zqc@192.168.0.109>
2020-07-16 07:23:53 +0800

    16日常规提交

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,23 +2,48 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="conta"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/DetailList'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '学生票',
+        children: [{
+          title: '成人三馆联票',
+          children: [{
+            title: '特别优惠'
+          }]
+        },{
+          title: '成人五馆联票'
+        }]
+      },{
+        title: '儿童票'
+      },{
+        title: '特惠票'
+      },{
+        title: '成人票'
+      }]
+    }
   }
 }
 </script>
 
 <style lang='stylus' scoped>
-  .conta
+  .content
     height: 20rem
 </style>
